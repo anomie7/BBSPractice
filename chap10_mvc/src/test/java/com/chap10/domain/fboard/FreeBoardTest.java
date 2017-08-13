0d76ababd48bf3f5e9eec061409b1c7e9453898f
minu youn <anomie7777@gmail.com>
2017-08-13 17:17:25 +0900

    chap10_mvc] 메이븐 다이나믹 프로젝트 설정 완료, logback 설정 완료

---
+++ chap10_mvc/src/test/java/com/chap10/domain/fboard/FreeBoardTest.java
@@ -0,0 +1,17 @@
+package com.chap10.domain.fboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FreeBoardTest {
+	private static final Logger logger = LoggerFactory.getLogger(FreeBoardTest.class);
+
+	@Test
+	public void test() {
+		logger.debug("안녕");
+	}
+
+}
